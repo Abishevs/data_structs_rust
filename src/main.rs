9a23522b8d1a8a8ce6cba734215fa30646eea7b5
abishevs <abisevs.eduards@gmail.com>
2023-10-21 10:47:46 +0200

    implemented an simple bracket balancing parser

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,57 @@
 mod stack;
-mod queue;
+use std::io;
 use stack::Stack;
-use queue::Queue;
 
-fn main() {
-    let mut test_stack = Stack::new();
-    let mut test_queue = Queue::new();
-    test_queue.push("BRW");
-    test_stack.push(2);
-    test_stack.push(5);
-    test_stack.push(3);
-    // neat way to print out the structure if attribute debbug is set of course 
-    println!("intial {:?}", test_stack);
-    println!("size of the stack: {}",test_stack.size());
-    match test_stack.peek(){
-        Some(value) => println!("Peek stack of stack: {:?}. peek:{}",test_stack, value),
-        None => println!("Stack is empty")
+fn matching(open: u8, close: u8) ->bool {
+    if close < open {
+        return false;
     }
-    test_stack.pop();
-    match test_stack.peek(){
-        Some(value) => println!("Peek stack after poping an element {}", value),
-        None => println!("Stack is empty")
+    match close - open {
+        2 | 1 => true,
+        _ => false,
     }
-    println!("size of the stack after poping one element: {}",test_stack.size());
-    test_stack.pop();
-    test_stack.pop();
-    println!("size of the stack after popping all elements: {}",test_stack.size());
-    test_stack.push(69);
-    match test_stack.peek(){
-        Some(value) => println!("Peek stack after adding an element {}", value),
-        None => println!("Stack is empty")
+}
+
+fn main(){
+    let mut stack = Stack::new();
+    let mut buffer = String::new();
+    match io::stdin().read_line(&mut buffer) {
+        Ok(n) => {
+            for byte in buffer.bytes() {
+                // 40 = ( 91 = [ 123 = { 
+                if byte == 40 || byte == 91 || byte == 123 { 
+                    stack.push(byte)
+                } else {
+                    match stack.peek() {
+                        Some(&peek) => {
+                            // check peek against current byte if
+                            // current byte - peek is 1 or 2 then they are matching 
+                            // ) = 41 ] = 93 and } = 125
+                            if matching(peek, byte) {
+                                // so then remove it from the stack
+                                stack.pop();
+                            } 
+                    }
+                    None => {println!("Contains non bracket symbol byte: {}", byte );
+                    break;
+                    }
+
+                }
+            }
+            }
+            println!("{n} bytes read");
+            println!("{}", buffer);
+        }
+        Err(error) => println!("error: {error}"),
     }
-    test_stack.pop();
-    match test_stack.peek(){
-        Some(value) => println!("Peek an empty stack {}", value),
-        None => println!("PEEKED Stack should be empty and None match is runeed")
+    // BUG: it has to have matching pairs else it will evalute to true
+    if stack.is_empty() {
+        // if they all where matching, then yeeeet
+        println!("{}", true);
+    }else {
+        // else baaaad
+        println!("{}", false);
     }
 
-    test_stack.peek();
-    println!("Last state of the stack(should be empty by now) {:?}", test_stack);
-
+    println!("{:?}", stack)
 }
